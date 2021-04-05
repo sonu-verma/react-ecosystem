2e38dc0720dd272d9d549715a18442f9a50bc44c
Sonu Verma <sonu.verma0715@gmail.com>
2021-04-05 22:04:30 +0530

    first commit

---
+++ extra.js
@@ -0,0 +1,3 @@
+//  to run server
+// node_modules/.bin/webpack serve
+// npm run start:dev
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./App.css";
+
+const App = () => ( 
+    <div className = "App" >
+        <h2> Hello Basic </h2> 
+    </div>
+);
+
+export default App;
